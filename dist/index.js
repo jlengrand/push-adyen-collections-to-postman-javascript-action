f81f441af18d76c20783861b12e6327bf4af17d3
Julien Lengrand-Lambert <julien@lengrand.fr>
2023-02-13 14:52:53 +0100

    Adding better inputs.
    * Also trying to run from main

--- dist/index.js
+++ dist/index.js
@@ -9779,8 +9779,12 @@ const github = __nccwpck_require__(5438);
 
 try {
     // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet');
-    console.log(`Hello ${nameToGreet}!`);
+    const postmanApiKey = core.getInput('postman-api-key');
+    const filesChanged = core.getInput('files-changed');
+
+    console.log(`Hello ${postmanApiKey}!`);
+    console.log(`Hello ${filesChanged}!`);
+
     const time = (new Date()).toTimeString();
     core.setOutput("time", time);
     // Get the JSON webhook payload for the event that triggered the workflow
--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const github = require('@actions/github');
 
 try {
     // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet');
-    console.log(`Hello ${nameToGreet}!`);
+    const postmanApiKey = core.getInput('postman-api-key');
+    const filesChanged = core.getInput('files-changed');
+
+    console.log(`Hello ${postmanApiKey}!`);
+    console.log(`Hello ${filesChanged}!`);
+
     const time = (new Date()).toTimeString();
     core.setOutput("time", time);
     // Get the JSON webhook payload for the event that triggered the workflow
