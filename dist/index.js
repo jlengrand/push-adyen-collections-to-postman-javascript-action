8743f239bb76fc15a817725f150ef28ad0bf2e4d
Julien Lengrand-Lambert <julien@lengrand.fr>
2023-03-08 11:05:19 +0100

    Wasn't working because I wasn't building?

--- dist/index.js
+++ dist/index.js
@@ -9777,21 +9777,40 @@ var __webpack_exports__ = {};
 const core = __nccwpck_require__(2186);
 const github = __nccwpck_require__(5438);
 
-try {
-    const postmanApiKey = core.getInput('postman-key');
-    const filesChanged = core.getInput('files-changed');
 
-    console.log(`Hello ${postmanApiKey}!`);
-    console.log(`Hello ${filesChanged}!`);
+async function run() {
 
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    // const payload = JSON.stringify(github.context.payload, undefined, 2)
-    // console.log(`The event payload: ${payload}`);
-} catch (error) {
-    core.setFailed(error.message);
+    try {
+        const postmanApiKey = core.getInput('postman-key');
+        const filesChanged = core.getInput('files-changed');
+        const workspaceId = core.getInput('workspace-id');
+
+        const zeFiles = filesChanged.split(' ');
+
+        console.log(`Hello ${postmanApiKey}!`);
+        console.log(`Hello ${workspaceId}!`);
+        console.log(`Hello ${filesChanged}!`);
+
+        // Get the JSON webhook payload for the event that triggered the workflow
+        // const payload = JSON.stringify(github.context.payload, undefined, 2)
+        // console.log(`The event payload: ${payload}`);
+
+        // Get the list of collections in the workspace
+        const collections = await getCollectionsInWorkspace(workspaceId, postmanApiKey);
+
+        console.log(`The collections: ${collections}`);
+
+        const time = (new Date()).toTimeString();
+        core.setOutput("time", time);
+
+
+    } catch (error) {
+        core.setFailed(error.message);
+    }
 }
+
+
+run()
 })();
 
 module.exports = __webpack_exports__;
---
+++ postmanLibrary.js
@@ -0,0 +1,42 @@
+const https = require('https');
+
+async function getCollectionsInWorkspace(workspaceId, postmanApiKey) {
+    const options = {
+        hostname: 'api.getpostman.com',
+        port: 443,
+        path: '/workspaces/'+workspaceId+'/collections',
+        method: 'GET',
+        headers: {
+            'X-Api-Key': postmanApiKey
+        }
+    }
+
+    return new Promise((resolve, reject) => {
+
+        const req = https.request(options, (res) => {
+            if (res.statusCode < 200 || res.statusCode >= 300) {
+                return reject(new Error('statusCode=' + res.statusCode));
+            }
+
+            let body = [];
+            res.on('data', (chunk) => {
+                body.push(chunk);
+            });
+
+            res.on('end', () => {
+                try {
+                    body = JSON.parse(Buffer.concat(body).toString());
+                } catch (e) {
+                    reject(e);
+                }
+                resolve(body);
+            });
+        });
+
+        req.on('error', (err) => {
+            reject(err);
+        });
+
+        req.end();
+    });
+}
